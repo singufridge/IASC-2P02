b7d63071d19fdae43c2a37b5918c0498269c7d4e
singufridge <>
2025-01-29 17:23:02 -0500

    Added prototype 2

--- prototype2/script.js
+++ prototype2/script.js
@@ -25,6 +25,7 @@ const camera = new THREE.PerspectiveCamera(
     100 //far plane
 )
 scene.add(camera)
+camera.position.set(0, 2, 6)
 
 //Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -34,7 +35,7 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 
 // Controls
-const controls = new OrbitControls(camera, renderer.canvas)
+const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
 /* *** MESHES *** */
@@ -44,11 +45,59 @@ const sphereMaterial = new THREE.MeshNormalMaterial()
 const testSphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
 
 scene.add(testSphere)
-testSphere.position.set(0, 0, -5)
+testSphere.position.set(0, 1, 0)
+
+//Plane
+const planeGeometry = new THREE.PlaneGeometry(10, 10, 50, 50)
+const planeMaterial = new THREE.MeshBasicMaterial({
+    color: new THREE.Color('white'),
+    side: THREE.DoubleSide,
+    wireframe: true
+})
+const plane = new THREE.Mesh(planeGeometry, planeMaterial)
+
+scene.add(plane)
+plane.rotation.x = Math.PI * 0.5
 
 /* *** UI *** */
 //UI
 const ui = new dat.GUI()
+
+//UI Object
+const uiObject = {
+    speed: 1,
+    distance: 1
+}
+
+//testSphere UI
+const sphereFolder = ui.addFolder('Sphere')
+
+sphereFolder
+    .add(testSphere.position, 'y')
+    .min(-5)
+    .max(5)
+    .step(1)
+    .name('Sphere Height')
+
+sphereFolder
+    .add(uiObject, 'speed')
+    .min(1)
+    .max(10)
+    .step(.1)
+    .name('Speed')
+
+sphereFolder
+    .add(uiObject, 'distance')
+    .min(1)
+    .max(5)
+    .step(.1)
+    .name('Distance')
+
+//plane ui
+const planeFolder = ui.addFolder('Plane')
+planeFolder
+    .add(planeMaterial, 'wireframe')
+    .name('Toggle Wireframe')
 
 /* *** ANIMATION LOOP *** */
 const clock = new THREE.Clock()
@@ -56,6 +105,10 @@ const clock = new THREE.Clock()
 const animation = () => {
     //Return elapsed time
     const elapsedTime = clock.getElapsedTime()
+
+    //animate sphere
+    testSphere.position.z = Math.sin(elapsedTime * uiObject.speed) * uiObject.distance
+    testSphere.position.x = Math.cos(elapsedTime * uiObject.speed) * uiObject.distance
 
     //Update OrbitControls
     controls.update()
