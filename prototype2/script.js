a678d2b0a74e1ab24380420eece8f7674db8ee91
singufridge <>
2025-01-30 17:28:02 -0500

    Added rotation speed and scale

--- prototype2/script.js
+++ prototype2/script.js
@@ -9,6 +9,7 @@ const sizes = {
     aspectRatio: window.innerWidth / window.innerHeight
 }
 
+
 /* *** SCENE *** */
 //Canvas
 const canvas = document.querySelector('.webgl')
@@ -38,14 +39,15 @@ renderer.setSize(sizes.width, sizes.height)
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-/* *** MESHES *** */
-// testSphere
-const sphereGeometry = new THREE.SphereGeometry(1)
-const sphereMaterial = new THREE.MeshNormalMaterial()
-const testSphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
 
-scene.add(testSphere)
-testSphere.position.set(0, 1, 0)
+/* *** MESHES *** */
+//Torus Knot
+const knotGeometry = new THREE.TorusKnotGeometry(.5, .2);
+const knotMaterial = new THREE.MeshNormalMaterial()
+const torusKnot = new THREE.Mesh(knotGeometry, knotMaterial)
+
+scene.add(torusKnot)
+torusKnot.position.set(0, 1, 0)
 
 //Plane
 const planeGeometry = new THREE.PlaneGeometry(10, 10, 50, 50)
@@ -59,6 +61,7 @@ const plane = new THREE.Mesh(planeGeometry, planeMaterial)
 scene.add(plane)
 plane.rotation.x = Math.PI * 0.5
 
+
 /* *** UI *** */
 //UI
 const ui = new dat.GUI()
@@ -66,38 +69,60 @@ const ui = new dat.GUI()
 //UI Object
 const uiObject = {
     speed: 1,
-    distance: 1
+    distance: 1,
+    scale: 1,
+    rotationSpeed: 1,
+    play: false
 }
 
 //testSphere UI
-const sphereFolder = ui.addFolder('Sphere')
+const knotFolder = ui.addFolder('Torus Knot')
 
-sphereFolder
-    .add(testSphere.position, 'y')
+knotFolder
+    .add(uiObject, 'play')
+    .name('Animate')
+
+knotFolder
+    .add(torusKnot.position, 'y')
     .min(-5)
     .max(5)
-    .step(1)
-    .name('Sphere Height')
+    .step(.5)
+    .name('Height')
 
-sphereFolder
+knotFolder
     .add(uiObject, 'speed')
     .min(1)
     .max(10)
     .step(.1)
     .name('Speed')
 
-sphereFolder
+knotFolder
     .add(uiObject, 'distance')
     .min(1)
     .max(5)
     .step(.1)
     .name('Distance')
 
+knotFolder
+    .add(uiObject, 'scale')
+    .min(1)
+    .max(5)
+    .step(.1)
+    .name('Scale')
+
+knotFolder
+    .add(uiObject, 'rotationSpeed')
+    .min(1)
+    .max(20)
+    .step(.1)
+    .name('Rotation Speed')
+
 //plane ui
 const planeFolder = ui.addFolder('Plane')
 planeFolder
     .add(planeMaterial, 'wireframe')
     .name('Toggle Wireframe')
+
 
 /* *** ANIMATION LOOP *** */
 const clock = new THREE.Clock()
@@ -106,9 +131,17 @@ const animation = () => {
     //Return elapsed time
     const elapsedTime = clock.getElapsedTime()
 
-    //animate sphere
-    testSphere.position.z = Math.sin(elapsedTime * uiObject.speed) * uiObject.distance
-    testSphere.position.x = Math.cos(elapsedTime * uiObject.speed) * uiObject.distance
+    //Animate torus knot
+    if(uiObject.play) {
+        torusKnot.position.z = Math.sin(elapsedTime * uiObject.speed) * uiObject.distance
+        torusKnot.position.x = Math.cos(elapsedTime * uiObject.speed) * uiObject.distance
+
+        torusKnot.scale.x = uiObject.scale
+        torusKnot.scale.y = uiObject.scale
+        torusKnot.scale.z = uiObject.scale
+
+        torusKnot.rotation.y = elapsedTime * uiObject.rotationSpeed
+    }
 
     //Update OrbitControls
     controls.update()
