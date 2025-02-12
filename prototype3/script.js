2b5819d161e63d73ce96eae53b7a660b2cc00531
singufridge <>
2025-02-12 18:25:17 -0500

    Added Prototype 3

---
+++ prototype3/script.js
@@ -0,0 +1,125 @@
+import * as THREE from "three";
+import * as dat from "lil-gui";
+import { OrbitControls } from "OrbitControls";
+
+/* *** SETUP *** */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    aspectRatio: window.innerWidth / window.innerHeight
+}
+
+/* *** SCENE *** */
+//Canvas
+const canvas = document.querySelector('.webgl')
+
+//Scene
+const scene = new THREE.Scene()
+scene.background = new THREE.Color("black")
+
+//Camera
+const camera = new THREE.PerspectiveCamera(
+    75, //fov
+    sizes.aspectRatio, //aspect ratio
+    0.1, //near plane
+    100 //far plane
+)
+scene.add(camera)
+camera.position.set(10, 2, 7.5)
+
+//Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas : canvas,
+    antialias : true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/* *** MESHES *** */
+// Cave
+const planeGeo = new THREE.PlaneGeometry(15.5, 7.5)
+const planeMat = new THREE.MeshStandardMaterial({
+    color: new THREE.Color('white'),
+    side: THREE.DoubleSide
+})
+const cave = new THREE.Mesh(planeGeo, planeMat)
+cave.rotation.y = Math.PI * .5;
+cave.receiveShadow = true
+scene.add(cave)
+
+// Objects
+// Torus knot
+const knotGeometry = new THREE.TorusKnotGeometry(1, 0.2);
+const knotMaterial = new THREE.MeshNormalMaterial()
+const torusKnot = new THREE.Mesh(knotGeometry, knotMaterial)
+
+torusKnot.position.set(6, 1, 0)
+torusKnot.castShadow = true
+scene.add(torusKnot)
+
+/* *** LIGHTS *** */
+// Ambient Light
+/*
+const ambientLight = new THREE.AmbientLight(
+    new THREE.Color('white')
+)
+scene.add(ambientLight)
+*/
+
+const directionalLight = new THREE.DirectionalLight(
+    new THREE.Color('white'),
+    0.5
+)
+scene.add(directionalLight)
+directionalLight.position.set(20, 4, 0)
+directionalLight.target = cave
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.width = 2048
+directionalLight.shadow.mapSize.height = 2048
+
+/* *** UI *** */
+//UI
+const ui = new dat.GUI()
+
+const lightPosFolder = ui.addFolder('Light Position')
+
+lightPosFolder
+    .add(directionalLight.position, 'y')
+    .min(-10)
+    .max(10)
+    .step(.1)
+    .name('Y')
+
+lightPosFolder
+    .add(directionalLight.position, 'z')
+    .min(-10)
+    .max(10)
+    .step(.1)
+    .name('Z')
+
+/* *** ANIMATION LOOP *** */
+const clock = new THREE.Clock()
+
+const animation = () => {
+    //Return elapsed time
+    const elapsedTime = clock.getElapsedTime()
+
+    // Animate Objects
+    torusKnot.rotation.y = elapsedTime;
+    torusKnot.rotation.z = elapsedTime;
+
+    //Update OrbitControls
+    controls.update()
+
+    //Render
+    renderer.render(scene, camera)
+
+    //Request next frame
+    window.requestAnimationFrame(animation) //runs function on a loop
+}
+animation()
